53292bea7b3f18885458e434d7ada65485c7419f
Yoga Hanggara <yohang88@gmail.com>
2020-08-14 08:52:18 +0700

    wording landing page
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -117,7 +117,7 @@ export default {
       menuItems: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Dashboard',
           to: '/',
           permission: 'access-backoffice'
         },
--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,19 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-flex>
+    <v-card>
+      <v-card-title>
+        Selamat Datang di Dashboard Pendaftaran Tes Masif PIKOBAR
+      </v-card-title>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   middleware: 'auth',
 
   components: {
-    Logo,
-    VuetifyLogo
+    //
   },
 
   created() {

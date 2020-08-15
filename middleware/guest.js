86ed5afe2a409278c4b5d0bc156d6cedb04d288b
Yoga Hanggara <yohang88@gmail.com>
2020-08-15 22:37:15 +0700

    temporary fix middleware guest (infinite loop redirect)
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,21 +1,3 @@
 export default ({ app, redirect }) => {
-  if (app.$keycloak.authenticated) {
-    return redirect('/')
-  }
-
-  return new Promise((resolve, reject) => {
-    app.$keycloak
-      .init({
-        onLoad: 'check-sso',
-        checkLoginIframe: true
-      })
-      .then((auth) => {
-        if (auth) {
-          return redirect('/')
-        }
-
-        resolve()
-      })
-      .catch(reject)
-  })
+  // @TODO implement better middleware
 }

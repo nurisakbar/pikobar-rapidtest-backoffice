1e183e3915a82b8eddc4a31dfc8e3fbf12d63ca6
Yoga Hanggara <yohang88@gmail.com>
2020-08-23 00:53:38 +0700

    wording revisi kemenkes v5
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- components/ApplicantsDatatable.vue
+++ components/ApplicantsDatatable.vue
@@ -430,13 +430,31 @@ export default {
         return 'OTG'
       }
 
+      if (value === 'CONFIRMED') {
+        return 'Konfirmasi'
+      }
+
+      if (value === 'SUSPECT') {
+        return 'Suspek'
+      }
+
+      if (value === 'PROBABLE') {
+        return 'Probable'
+      }
+
+      if (value === 'CLOSE_CONTACT') {
+        return 'Kontak Erat'
+      }
+
       if (value === 'NOT_ALL') {
-        return 'Bukan Ketiganya'
+        return 'Bukan Semuanya'
       }
 
       if (value === 'UNKNOWN') {
         return 'Tidak Tahu'
       }
+
+      return value
     }
   }
 }
